f8bc9d5eab2e00051e7e63b2c84686a5ffac2931
Neeraj Kumar <neerajarya08243@gmail.com>
2019-07-31 12:03:58 +0530

    fix share flow

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <meta property="og:url" content="https://rakhi2cm.com"/>
+    <meta property="og:url" content="{{$rakhi->imageUrl}}"/>
     <meta property="og:type" content="article"/>
     <meta property="og:title" content="जेव्हा ग्रेट दिमाइन्स एकसारखे विचार करत नाहीत"/>
     <meta property="og:description" content="संस्कृतीच्या रचनात्मक विचारांवर किती प्रभाव पडतो?"/>
